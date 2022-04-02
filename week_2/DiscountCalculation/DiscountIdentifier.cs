4e23398d5b6184eb982cf86530473135243d9abc
almaz.asanbek44@gmail.com <almaz.asanbek44@gmail.com>
2022-04-02 13:41:56 +0600

    init

---
+++ week_2/DiscountCalculation/DiscountIdentifier.cs
@@ -0,0 +1,48 @@
+﻿using DiscountCalculation.Entities;
+using DiscountCalculation.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace DiscountCalculation
+{
+    public class DiscountIdentifier
+    {
+        private delegate double Calculate(double amount, int discount);
+       
+        private double CalculateDiscount(double x, int y) => x - (x* y / 100);
+
+        public Dictionary<CustomerType, int> discountStrategy { get; private set; } 
+
+        public DiscountIdentifier()
+        {
+            discountStrategy = new Dictionary<CustomerType, int>();
+        }
+        public double IdentifyDiscount(Customer customer)
+        {
+            Calculate calculateDisc = new Calculate(CalculateDiscount);
+            int disc = 0;
+            double result;
+            switch ((int)customer.CustomerType)
+            {
+                case 1:
+                    disc = discountStrategy[customer.CustomerType];
+                    break;
+                case 2:
+                    if (customer.Order.Amount > 100000)
+                        disc = 15;
+                    else if(0 < customer.Order.Amount && customer.Order.Amount < 100001)
+                        disc = discountStrategy[customer.CustomerType];
+                    break;
+                case 3:
+                    disc  = discountStrategy[customer.CustomerType];
+                    break;
+            }
+             result = calculateDisc(customer.Order.Amount, disc);
+            if (customer.CustomerType == CustomerType.NewCustomer)
+                Console.WriteLine("There is no discount for new customer , amount to be paid remain same {0}", result);
+            else
+                Console.WriteLine("Discount will be {0}%, amount before discount = {1}. After discount amount to be paid = {2}", disc, customer.Order.Amount, result);
+            return result;
+        }
+    }
+}
---
+++ week_2/DiscountCalculation/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using DiscountCalculation.Enums;
+
+namespace DiscountCalculation.Entities
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerType CustomerType { get; set; }
+        public  Order Order { get; private set; }
+        public Customer(Order order)
+        {
+            Order = order;
+        }
+    }
+}
