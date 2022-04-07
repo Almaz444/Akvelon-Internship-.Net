2da2af3d4c2c9e5c7aaa42983c62d67ba52316c7
almaz.asanbek44@gmail.com <almaz.asanbek44@gmail.com>
2022-04-08 03:32:20 +0600

    init

---
+++ week_2/DisposeTask/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DisposeTask
+{
+    public class Customer : IDisposable
+    {
+        private StringReader _reader;
+        private bool isDisposed = false;
+        public Customer()
+        {
+            this._reader = new StringReader("test");
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposeValue)
+        {
+            if (!isDisposed)
+            {
+                if (disposeValue)
+                {
+                    if (_reader != null)
+                    {
+                        this._reader.Dispose();
+                    }
+                }
+
+                isDisposed = true;
+            }
+        }
+    }
+}
---
+++ week_2/DisposeTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisposeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
