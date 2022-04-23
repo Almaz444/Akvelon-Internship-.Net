5efe0e7dea325d36a5f602d66786547e291dd7de
almaz.asanbek44@gmail.com <almaz.asanbek44@gmail.com>
2022-04-23 17:16:02 +0600

    init

---
+++ week_4/Task3/ImageDownload/Downloader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ImageDownload
+{   
+    public class Downloader
+    {
+        public async Task DownloadImage(string url, string destinationFolder)
+        {
+            try
+            {
+                Uri uri = new Uri(url);
+                string destinationFilePath = Path.Combine(destinationFolder, uri.Segments.Last());
+                if (File.Exists(destinationFilePath))
+                {
+                    File.Delete(destinationFilePath);
+                }
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    byte[] fileBytes = await httpClient.GetByteArrayAsync(url);
+                    File.WriteAllBytes(destinationFilePath, fileBytes);
+                    Console.WriteLine("Download is completed.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to download File: " + e.Message);
+            }
+        }
+    }
+}
---
+++ week_4/Task3/ImageDownload/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageDownload
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Downloader downloader = new Downloader();
+            string url = "https://www.gstatic.com/webp/gallery3/3_webp_ll.png";
+            Console.WriteLine("Kindly type name of folder on disc 'C' in order to save image.");
+            string temp = Console.ReadLine();
+            try
+            {
+                string folderName = @"C:\"+temp;
+                if (!Directory.Exists(folderName))
+                {
+                    Directory.CreateDirectory(folderName);
+                }
+                await downloader.DownloadImage(url, @"c:\" + temp);
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine(ioex.Message);
+            }
+        }
+    }
+}
